286d3648faf28fe582823eef80fdbfb54689683f
DinhDuong Tran <trandinhduong24072003@gmail.com>
2026-03-10 08:09:16 +0700

    fix excercise1

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,7 +4,7 @@ export default function Banner(){
             <div className="banner-over-lay w-full h-full"></div>
             <div className="banner-content absolute w-full h-full flex flex-col items-center justify-center ">
                 <h1 className="banner-content-title">Hello Developer!</h1>
-                <h2 className="banner-content-description"> Lorem ipsum dolor sit amet, consectetur adipisicing elit</h2>
+                <h2 className="banner-content-description font-normal tracking-wide"> Lorem ipsum dolor sit amet, consectetur adipisicing elit</h2>
             </div>
         </div>
     );
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,7 +3,7 @@ import imgBreak from "../assets/img/400x300.svg"
 export default function Post({content,postImg}){
     return(
         <div className="post-item border border-primary-light shadow-sm rounded-sm p-4 m-3 max-w-[600px] flex flex-col" >
-            <img className="post-item-img mb-3" src={postImg?postImg:imgBreak} alt="Card image cap"/>
+            <img className="post-item-img mb-3 rounded-sm" src={postImg?postImg:imgBreak} alt="Card image cap"/>
             <div className="post-body flex flex-col flex-1">
                 <p className="post-text my-4 flex-1" dangerouslySetInnerHTML={{ __html: content }}></p>
                 <a href="#" className="btn btn-primary read-btn mt-auto">READ MORE</a>
